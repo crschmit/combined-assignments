26f69c48b92c44af941d8abb60f6e2b9b341e8fa
crschmit <crschmit@gmail.com>
2017-06-05 11:24:29 -0700

    Added Check.java -- Check.java unmarshalls student.xml and config.xml

---
+++ 5-socket-io-serialization/src/main/java/com/cooksys/ftd/assignments/socket/Check.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package com.cooksys.ftd.assignments.socket;
+
+import java.io.File;
+import java.io.FileInputStream;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+
+import com.cooksys.ftd.assignments.socket.model.Config;
+import com.cooksys.ftd.assignments.socket.model.Student;
+
+/**
+ * @author ftd-8
+ *
+ */
+public class Check {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Student s;
+		Config c;
+		File sx;
+		File cx;
+		FileInputStream sxin;
+		FileInputStream cxin;
+		JAXBContext ctx;
+		
+		try {
+			sx =  new File("config/student.xml");
+			sxin = new FileInputStream(sx);
+			ctx = JAXBContext.newInstance(Student.class);
+			Unmarshaller u = ctx.createUnmarshaller();
+			s = (Student) u.unmarshal(sxin);
+			
+			System.out.print("Student: " + s.getLastName() + ", " + s.getFirstName() + "\n"
+							+ "\tFavorite Language: " + s.getFavoriteLanguage() + "\n"
+							+ "\tFavorite Programming Paradigm: " + s.getFavoriteParadigm() + "\n"
+							+ "\tFavorite IDE: " + s.getFavoriteIDE() + "\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			cx =  new File("config/config.xml");
+			cxin = new FileInputStream(cx);
+			ctx = JAXBContext.newInstance(Config.class);
+			Unmarshaller u = ctx.createUnmarshaller();
+			c = (Config) u.unmarshal(cxin);
+			
+			System.out.print("Config:\n"
+						   + "\tLocal Config:\n" 
+						   + "\t\tPort: " + c.getLocal().getPort() + "\n"
+						   + "\tRemote Config:\n"
+						   + "\t\tHost: " + c.getRemote().getHost() + "\n"
+						   + "\t\tPort: " + c.getRemote().getPort() + "\n"
+						   + "\tStudent File Path: " + c.getStudentFilePath() + "\n");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
